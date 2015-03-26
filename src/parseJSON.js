0c37f54d9228cdabb9ed688a4ce49cc35a9a3fcc
Cody Scholberg <cscholberg@gmail.com>
2015-03-26 09:57:51 -0400

    added some more lines. nothing is working yet that I can see.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -11,6 +11,34 @@ var parseJSON = function(json) {
         return true;
     } else if(json === 'false'){
         return false;
-    } 
+    } else if(json === 'null'){
+        return null;
+    } else if( (json[0] === '"' && json[json.length - 1] === '"')||(json[0] === "'" && json[json.length - 1] === "'") ){
+        var output = '';
+        for(var i = 1; i < json.length - 1; i++){
+            output += json[i];
+        }
+        return output;
+    } else if(json[0] === '[' && json[json.length - 1] === ']'){
+        var arr = [];
+        for(var i = 1; i < json.length - 1; i++){
+            arr.push(parseJSON(json[i]));
+            i++;
+        }
+        return arr;
+    } else if(json[0] === '{' && json[json.length - 1] === '}'){
+        var obj = {};
+        var str = '';
+        for(var i = 1; i < json.length - 1; i++){
+            str += parseJSON[i];
+        }
+        str = str.split(':');
+        return str[0] + ':' + str[1];
+    } else {
+        throw "SyntaxError";
+    }
+
+    
+  }
 
 };
