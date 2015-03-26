ad283b63827d6d5d5cf9801d2fc778a06d781819
Cody Scholberg <cscholberg@gmail.com>
2015-03-26 07:41:39 -0400

    Stringify JSON. Not working. Fixed numerous errors. No clue how long I have left.  Very difficult.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var output;
+    if(typeof(obj) ==='undefined'){
+        return;
+    } else if(obj === null){
+        return obj + '';
+    } else if(typeof(obj) === 'function'){
+        return;
+    } else if(typeof(obj) === 'number'){
+        return obj + '';
+    } else if(typeof(obj) === 'boolean'){
+        return obj + '';
+    } else if(typeof(obj) === 'string'){
+        return "\"" + obj + "\"";
+    } else if(Array.isArray(obj)){
+        output = [];
+        _.each(obj, function(item){
+            output.push(stringifyJSON(item));
+        });
+        return "[" + output + "]";
+    } else if(typeof(obj) === 'object'){
+        output = '';
+        _.each(obj, function(value, key){
+            var left = stringifyJSON(key);
+            var right = stringifyJSON(value);
+            if(typeof(right) !== undefined){
+                output = output + left + ":" + right + ",";   
+            }
+        });
+        return "{" + output.slice(0, -1) + "}";
+    }
+    
 };
